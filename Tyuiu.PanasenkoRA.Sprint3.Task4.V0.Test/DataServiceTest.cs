609c1c5c5c53e4d84662d8ff669c6cd18d3ba52a
Tyuiu.PanasenkoRA@outlook.com <panasenkoroma@mail.ru>
2023-10-25 22:29:14 +0500

    Task4

---
+++ Tyuiu.PanasenkoRA.Sprint3.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PanasenkoRA.Sprint3.Task4.V0.Lib;
+
+namespace Tyuiu.PanasenkoRA.Sprint3.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PanasenkoRA.Sprint3.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PanasenkoRA.Sprint3.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PanasenkoRA.Sprint3.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a676814b-aac5-4219-b7bf-3794d61f72e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
